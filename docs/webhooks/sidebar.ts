1bbaa10c6b4ec3c011fc616a2540922cf29ff342
Aniketpf <aniket.saha@prismforce.ai>
2026-03-16 11:12:01 +0530

    fix: ordering of the api and webhooks

--- docs/webhooks/sidebar.ts
+++ docs/webhooks/sidebar.ts
@@ -9,6 +9,82 @@ const sidebar: SidebarsConfig = {
         },
         {
             type: 'category',
+            label: 'Job Assessment Events',
+            collapsed: false,
+            items: [
+                {
+                    type: 'doc',
+                    id: 'webhooks/job-description-parse-success',
+                    label: 'job_description.parse.succeeded',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/job-description-parse-failed',
+                    label: 'job_description.parse.failed',
+                },
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Candidate Events',
+            collapsed: false,
+            items: [
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-creation-success',
+                    label: 'candidate.creation.succeeded',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-creation-failed',
+                    label: 'candidate.creation.failed',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-update-success',
+                    label: 'candidate.update.succeeded',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-update-failed',
+                    label: 'candidate.update.failed',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-resume-parsing-success',
+                    label: 'candidate.resume.parsing.succeeded',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-resume-parsing-failed',
+                    label: 'candidate.resume.parsing.failed',
+                },
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Candidate Assessment Events',
+            collapsed: false,
+            items: [
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-assessment-creation-success',
+                    label: 'candidate_assessment.creation.succeeded',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-assessment-creation-failed',
+                    label: 'candidate_assessment.creation.failed',
+                },
+                {
+                    type: 'doc',
+                    id: 'webhooks/candidate-assessment-interview-ready',
+                    label: 'candidate_assessment.interview.ready',
+                },
+            ],
+        },
+        {
+            type: 'category',
             label: 'Interview Events',
             collapsed: false,
             items: [
@@ -51,82 +127,6 @@ const sidebar: SidebarsConfig = {
                     type: 'doc',
                     id: 'webhooks/proctoring-data-generated',
                     label: 'proctoring.data.generated',
-                },
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Job Assessment Events',
-            collapsed: false,
-            items: [
-                {
-                    type: 'doc',
-                    id: 'webhooks/job-description-parse-success',
-                    label: 'job_description.parse.succeeded',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/job-description-parse-failed',
-                    label: 'job_description.parse.failed',
-                },
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Candidate Assessment Events',
-            collapsed: false,
-            items: [
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-assessment-creation-success',
-                    label: 'candidate_assessment.creation.succeeded',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-assessment-creation-failed',
-                    label: 'candidate_assessment.creation.failed',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-assessment-interview-ready',
-                    label: 'candidate_assessment.interview.ready',
-                },
-            ],
-        },
-        {
-            type: 'category',
-            label: 'Candidate Events',
-            collapsed: false,
-            items: [
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-creation-success',
-                    label: 'candidate.creation.succeeded',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-creation-failed',
-                    label: 'candidate.creation.failed',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-update-success',
-                    label: 'candidate.update.succeeded',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-update-failed',
-                    label: 'candidate.update.failed',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-resume-parsing-success',
-                    label: 'candidate.resume.parsing.succeeded',
-                },
-                {
-                    type: 'doc',
-                    id: 'webhooks/candidate-resume-parsing-failed',
-                    label: 'candidate.resume.parsing.failed',
                 },
             ],
         },
