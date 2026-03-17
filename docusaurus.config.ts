b022d930075b0f2772a7f0ae9c509abe9d74472f
Aniketpf <aniket.saha@prismforce.ai>
2026-03-17 13:59:23 +0530

    changed the base path to /docs/

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
     url: process.env.PRODUCTION_URL ?? 'http://localhost:3000',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/docs',
+    baseUrl: '/docs/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
